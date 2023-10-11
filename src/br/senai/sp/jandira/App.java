5178921537f24cad84dc68cccfc254ef5823112d
natybastazini <natybastazini7@gmail.com>
2023-10-11 16:56:31 -0300

    surtando!!

--- src/br/senai/sp/jandira/App.java
+++ src/br/senai/sp/jandira/App.java
@@ -7,6 +7,8 @@ public class App {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.cadastro();
+        animal.adicionarAnimal(animal);
+        animal.mostrarAnimais();
     }
 
 }
--- src/br/senai/sp/jandira/model/Animal.java
+++ src/br/senai/sp/jandira/model/Animal.java
@@ -1,5 +1,9 @@
 package br.senai.sp.jandira.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Animal {
@@ -11,6 +15,10 @@ public class Animal {
     String sexo;
     double id;
     int idade;
+
+    LocalDate localDate = LocalDate.now();
+    LocalDateTime localDateTime  = LocalDateTime.now();
+    List <Animal> listaDeAnimais = new ArrayList<>();
 
     public void cadastro(){
         System.out.println("//----------------------------------------//");
@@ -24,11 +32,27 @@ public class Animal {
         idade = scanner.nextInt();
         System.out.println("Informe o id: ");
         id = scanner.nextDouble();
+    }
 
+    public void adicionarAnimal(Animal animal) {
+        listaDeAnimais.add(animal);
+    }
 
+    public void mostrarAnimais () {
+        if (!listaDeAnimais.isEmpty()) {
+            for (Animal animal : listaDeAnimais) {
+                System.out.println("****************************************");
+                System.out.println("O nome do animal: " + animal.nome);
+                System.out.println("O sexo do animal: " + animal.sexo);
+                System.out.println("A idade do animal: " + animal.idade);
+                System.out.println("A espécie do animal: " + animal.especie);
+                System.out.println("****************************************");
+            }
+        } else {
+            System.out.println("ERRO: Não há nenhum animal cadastrado!!");
+        }
     }
 
 
-
-}
+    }
 
